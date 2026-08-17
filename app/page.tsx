b6251da114a8dd4dae5cb3e8d1521efb6a31d08c
hicham <relaxingchannel65@gmail.com>
2026-08-17 15:06:47 +0100

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Hero } from "@/components/Main/hero";
+import { HowItWorks } from "@/components/Main/how-it-works";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+    </>
+  );
 }
